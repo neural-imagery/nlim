28502a24e5f15d3a557971b4127ba8bbe0e1b378
Stephen Polcyn <spolcyn@users.noreply.github.com>
2023-10-07 20:59:59 -0400

    Clean up unused software

--- nlim/web/constants.py
+++
@@ -1,7 +0,0 @@
-from pathlib import Path
-
-UPLOADS_DIR = Path.home() / "datasets"
-METADATA_PATH = UPLOADS_DIR / "metadata.csv"
-
-UPLOADS_DIR = str(UPLOADS_DIR)
-METADATA_PATH = str(METADATA_PATH)
--- nlim/web/test.py
+++
@@ -1,7 +0,0 @@
-import requests
-
-requests.post(
-    "http://localhost:5000/dataset/add",
-    data={"metadata": "foo.txt test"},
-    files={"fnirs_data": ("foo.txt", open("foo.txt", "rb"))},
-)
